281290dca9bd1a863e9177e9252d09eee6ffbe16
HuntsmanRoym <grins008@morris.umn.edu>
2018-11-27 11:27:26 -0600

    Refined client-server communication

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -2,35 +2,53 @@ package segmentedfilesystem;
 
 import java.io.*;
 import java.util.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Arrays;
 
 
 public class Main {
+/*
+https://docs.oracle.com/javase/7/docs/api/java/net/DatagramPacket.html
 
-    public static void main(String[] args) {
+https://docs.oracle.com/javase/7/docs/api/java/net/DatagramSocket.html#receive(java.net.DatagramPacket)
+
+https://docs.oracle.com/javase/7/docs/api/java/net/DatagramSocketImpl.html
+
+https://www.geeksforgeeks.org/java-net-datagramsocket-class-java/
+*/
+    public static void main(String[] args) throws exception {
+      //Datagram socket constructor
+      DatagramSocket socket = new DatagramSocket();
+      InetAddress address = "heartofgold.morris.umn.edu"
+      int port = 6014;
+      int length = 1028;
+      byte buf[] = { };
+      byte buf1[] = new byte[4];
+      DatagramPacket dpOUT = new DatagramPacket(buf, 4, length, address, port);
+      DatagramPacket dpIN = new DatagramPacket(buf1, 4);
+
+      socket.connect(address, port);
+
+      socket.send(dpOUT);
+
+      //Make loop for receiving packets - Below is just for now
+      socket.receive(dpIN);
+
 
     }
 
 }
 
-public class ServerTalk {
-  int port = 6014;
-  int length = 1028;
-  char server = "heartofgold.morris.umn.edu";
-
-  Buffer buf = new Buffer();
-
-  DatagramPacket Converser = new DatagramPacket(byte[], buf, length, server, port);
-
-  DatagramSocket dgs = new DatagramSocket(port, server);
-
-}
 
 public class Process {
 
 ArrayList<Integer> A1 = new ArrayList<Integer>();
 ArrayList<Integer> A2 = new ArrayList<Integer>();
 ArrayList<Integer> A3 = new ArrayList<Integer>();
-
-
+//if (dpIN.getData() == number thing)
+if (receive )
 
 }
