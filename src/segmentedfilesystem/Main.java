5fa60f7102618a4779fb0403233aee8daa3f8af3
HuntsmanRoym <grins008@morris.umn.edu>
2018-11-27 11:03:31 -0600

    Set up beginning structure of client and data structure

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,36 @@
 package segmentedfilesystem;
 
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
     }
+
+}
+
+public class ServerTalk {
+  int port = 6014;
+  int length = 1028;
+  char server = "heartofgold.morris.umn.edu";
+
+  Buffer buf = new Buffer();
+
+  DatagramPacket Converser = new DatagramPacket(byte[], buf, length, server, port);
+
+  DatagramSocket dgs = new DatagramSocket(port, server);
+
+}
+
+public class Process {
+
+ArrayList<Integer> A1 = new ArrayList<Integer>();
+ArrayList<Integer> A2 = new ArrayList<Integer>();
+ArrayList<Integer> A3 = new ArrayList<Integer>();
+
+
 
 }
